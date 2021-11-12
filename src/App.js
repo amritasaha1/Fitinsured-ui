147884c2ad5577b492e1620ea65737ad210c131a
Oliver D'Costa <oliver.d-costa@capgemini.com>
2021-11-12 14:03:30 -0500

    UI Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to FITINSURED!
         </p>
         <a
           className="App-link"
@@ -15,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Chat with us
         </a>
       </header>
     </div>
